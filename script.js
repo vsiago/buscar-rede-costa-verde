1789225f693c83f0eee213c016548928d7aa6e43
vsiago <101620032+vsiago@users.noreply.github.com>
2023-05-25 10:37:19 +0000

    Persistindo os dados no local sotrage corretamente e clean code

--- script.js
+++ script.js
@@ -1,149 +1,96 @@
+// Elementos DOM
 const divContent = document.querySelector('.content');
-const divListFavoritos = document.querySelector('.lista-favoritos')
+const divListFavoritos = document.querySelector('.lista-favoritos');
 const inputSearch = document.querySelector('input[type="search"]');
-const btnLimpar = document.querySelector('.limpar')
-const url = './rede-costa-verde.json';
-let items = [];
-let itemsFavoritos = []
-
-// ------------------------------------------------------------------------------------
-
-// Realizar a requisição inicial para obter os dados
-fetch(url)
-  .then(response => response.json())
-  .then(redeJson => {
-    items = redeJson.map(rede => ({
-      nome: rede.nome,
-      seguimento: rede.seguimento,
-      telefone: rede.telefone,
-      regiao: rede.regiao,
-      endereco: rede.endereco,
-      princServicos: rede.princServicos
-    }));
-  });
-
-// ------------------------------------------------------------------------------------
-
-// Listar pelo botao Buscar Toda Rede
+const btnLimpar = document.querySelector('.limpar');
 const buscaRede = document.querySelector('#buscar-rede');
+const inputSelect = document.querySelector('#inputSelect');
 
-buscaRede.addEventListener('click', function() {
-    const searchTerm = inputSearch.value.toLowerCase();
-    const filteredItems = items.filter(item =>
-        item.regiao.toLowerCase().includes(searchTerm)
-    );
+// Constantes
+const url = './rede-costa-verde.json';
 
-    renderItems(filteredItems);
-})
+// Variáveis
+let items = [];
+let itemsFavoritos = [];
 
-// ------------------------------------------------------------------------------------
-
-//Evento de input para realizar a busca principal Por Regiao
-inputSearch.addEventListener('input', function() {
-    const searchTerm = inputSearch.value.toLowerCase();
-    const filteredItems = items.filter(item =>
-        item.regiao.toLowerCase().includes(searchTerm)
-    );
-
-    renderItems(filteredItems);
-
-    if(searchTerm === '') {
-        divContent.innerHTML = ''
-    }
-});
-
-// ------------------------------------------------------------------------------------
-
-// Listar por Opcao Selecionada
-function selecionarOpcao(selectElement) {
-    const opcaoSelecionada = selectElement.value
-
-    //Logica para buscar por regiao
-    if(opcaoSelecionada === 'por-regiao') {
-        inputKey.placeholder = 'Encontre pelo nome do Municipio';
-
-        inputSearch.addEventListener('input', function() {
-            const searchTerm = inputSearch.value.toLowerCase();
-            const filteredItems = items.filter(item =>
-                item.regiao.toLowerCase().includes(searchTerm)
-            );
-        
-            renderItems(filteredItems);
-        
-            if(searchTerm === '') {
-                divContent.innerHTML = ''
-            }
-        });
-    }
-
-    // Logica para buscar por especialidade
-    if(opcaoSelecionada === 'por-especialidade') {
-        inputKey.placeholder = 'Encontre por especialidade ou exame';
-
-        inputSearch.addEventListener('input', function() {
-            const searchTerm = inputSearch.value.toLowerCase();
-          
-            const filteredItems = items.filter(item =>
-              item.princServicos.some(servico => 
-                servico.toLowerCase().includes(searchTerm))
-            );
-          
-            renderItems(filteredItems);
-
-            if(searchTerm === '') {
-                divContent.innerHTML = ''
-            }
-          });
-    }
+// Função principal: inicialização do aplicativo
+function initializeApp() {
+  fetchItems();
+  setupEventListeners();
+  loadItemsFavoritosFromLocalStorage();
 }
 
-// Chamada da função selecionarOpcao após a definição
-document.querySelector('#inputSelect').addEventListener('change', function() {
-    selecionarOpcao(this);
-});
-
-// ------------------------------------------------------------------------------------
-
-// Funcao para limpar pesquisa
-btnLimpar.addEventListener('click', () => {
-  divContent.innerHTML = ''
-})
-
-// ------------------------------------------------------------------------------------
-
-// Funcao de salvar o item
-function salvarItem(item) {
-  divListFavoritos.innerHTML = ''
-  const index = itemsFavoritos.findIndex(favorito => favorito.id === item.id);
-
-  itemsFavoritos.push(item);
-  itemsFavoritos.forEach(itemFavorito => {
-    const { nome, seguimento, telefone, regiao, endereco, princServicos } = itemFavorito;
-
-    const li = document.createElement('li');
-    li.innerHTML += `
-      <h3 class="regiao">${regiao}</h3>
-      <p class="nome"><strong>${nome}</strong></p>
-      <p class="seguimento">${seguimento}</p>
-      <p class="telefone">${telefone}</p>
-      <p class="endereco">${endereco}</p>
-      <p class="princServicos">${princServicos}</p>
-    `;
-
-    divListFavoritos.append(li)
-  })
-  console.log(itemsFavoritos);
+// Função para buscar os dados
+function fetchItems() {
+  fetch(url)
+    .then(response => response.json())
+    .then(redeJson => {
+      items = redeJson.map(rede => ({
+        nome: rede.nome,
+        seguimento: rede.seguimento,
+        telefone: rede.telefone,
+        regiao: rede.regiao,
+        endereco: rede.endereco,
+        princServicos: rede.princServicos
+      }));
+    })
+    .catch(error => {
+      console.error('Erro ao buscar os dados:', error);
+    });
 }
 
-// Função para renderizar os itens salvos na lista de Favoritos
-function renderItems(filteredItems) {
+// Função para configurar os event listeners
+function setupEventListeners() {
+  buscaRede.addEventListener('click', buscarRede);
+  inputSearch.addEventListener('input', buscarRede);
+  inputSelect.addEventListener('change', selecionarOpcao);
+  btnLimpar.addEventListener('click', limparPesquisa);
+}
+
+// Função para buscar a rede
+function buscarRede() {
+  const searchTerm = inputSearch.value.toLowerCase();
+
+  let filteredItems;
+  if (inputSelect.value === 'por-regiao') {
+    filteredItems = items.filter(item =>
+      item.regiao.toLowerCase().includes(searchTerm)
+    );
+  } else if (inputSelect.value === 'por-especialidade') {
+    filteredItems = items.filter(item =>
+      item.princServicos.some(servico =>
+        servico.toLowerCase().includes(searchTerm)
+      )
+    );
+  }
+
+  renderItems(filteredItems);
+}
+
+// Função para selecionar a opção
+function selecionarOpcao() {
+  if (inputSelect.value === 'por-regiao') {
+    inputSearch.placeholder = 'Encontre pelo nome do Município';
+  } else if (inputSelect.value === 'por-especialidade') {
+    inputSearch.placeholder = 'Encontre por especialidade ou exame';
+  }
+
+  buscarRede();
+}
+
+// Função para limpar a pesquisa
+function limparPesquisa() {
+  divContent.innerHTML = '';
+}
+
+// Função para renderizar os itens encontrados
+function renderItems(items) {
   divContent.innerHTML = '';
 
-  filteredItems.forEach(item => {
+  items.forEach(item => {
     const { nome, seguimento, telefone, regiao, endereco, princServicos } = item;
 
-    const li = document.createElement('li');
-    li.innerHTML = `
+    const li = createListItemElement(`
       <h3 class="regiao">${regiao}</h3>
       <p class="nome"><strong>${nome}</strong></p>
       <p class="seguimento">${seguimento}</p>
@@ -151,17 +98,81 @@ function renderItems(filteredItems) {
       <p class="endereco">${endereco}</p>
       <p class="princServicos">${princServicos}</p>
       <button class="salvar-local">Salvar local</button>
-    `;
+    `);
 
-    divContent.append(li);
+    li.querySelector('.salvar-local').addEventListener('click', () => {
+      salvarItem(item);
+    });
 
-    const btnSalvarLocal = li.querySelector('.salvar-local');
-    btnSalvarLocal.addEventListener('click', function() {
-      salvarItem(item)
-    })
+    divContent.appendChild(li);
   });
 }
 
-// ------------------------------------------------------------------------------------
+// Função para criar um elemento de lista
+function createListItemElement(html) {
+  const li = document.createElement('li');
+  li.innerHTML = html;
+  return li;
+}
 
-console.log(itemsFavoritos);
+// Função para salvar um item nos favoritos
+function salvarItem(item) {
+  const enderecoExistente = itemsFavoritos.some(
+    favorito => favorito.endereco === item.endereco
+  );
+
+  if (enderecoExistente) {
+    return;
+  }
+
+  itemsFavoritos.unshift(item);
+  saveItemsFavoritosToLocalStorage();
+  renderItemsFavoritos();
+}
+
+// Função para renderizar os itens favoritos
+function renderItemsFavoritos() {
+  divListFavoritos.innerHTML = '';
+
+  itemsFavoritos.reverse()
+
+  for (let i = 0; i < itemsFavoritos.length; i++) {
+    const { nome, seguimento, telefone, regiao, endereco, princServicos } = itemsFavoritos[i];
+
+    const li = createListItemElement(`
+      <h3 class="regiao">${regiao}</h3>
+      <p class="nome"><strong>${nome}</strong></p>
+      <p class="seguimento">${seguimento}</p>
+      <p class="telefone">${telefone}</p>
+      <p class="endereco">${endereco}</p>
+      <p class="princServicos">${princServicos}</p>
+    `);
+
+    divListFavoritos.prepend(li);
+  }
+}
+
+// Função para salvar os itens favoritos no Local Storage
+function saveItemsFavoritosToLocalStorage() {
+  try {
+    localStorage.setItem('itemsFavoritos', JSON.stringify(itemsFavoritos));
+  } catch (error) {
+    console.error('Erro ao salvar no Local Storage:', error);
+  }
+}
+
+// Função para carregar os itens favoritos do Local Storage
+function loadItemsFavoritosFromLocalStorage() {
+  try {
+    const storedItems = localStorage.getItem('itemsFavoritos');
+    if (storedItems) {
+      itemsFavoritos = JSON.parse(storedItems);
+      renderItemsFavoritos();
+    }
+  } catch (error) {
+    console.error('Erro ao carregar do Local Storage:', error);
+  }
+}
+
+// Inicializar o aplicativo
+initializeApp();
